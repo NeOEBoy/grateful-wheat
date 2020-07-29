e165ef8faa2cf8c4e36d4fd3bd08a2d3bde04106
NeO <wangronghui2012@hotmail.com>
2020-07-29 12:14:45 +0800

    端口号修改一下，避免和儒越的端口冲突

--- api/pm2.config.js
+++ api/pm2.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   apps: [{
     name: "grateful-wheat-api-production",
-    script: "./bin/www"
+    script: "./bin/www",
+    env: {
+      "NODE_ENV": "production",
+      "PORT": "9000"
+    }
   }]
 }

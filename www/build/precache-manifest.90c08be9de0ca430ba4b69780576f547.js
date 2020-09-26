9bc644d02f9987163c94317cc54027d74ba9b888
NeO <wangronghui2012@hotmail.com>
2020-09-26 18:16:39 +0800

    提交build

---
+++ www/build/precache-manifest.90c08be9de0ca430ba4b69780576f547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a099dd4e5b817cef8774fbe7b1a375d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16503d581b394a50cc6d",
+    "url": "/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "7cea66aaa8cba6e88cea",
+    "url": "/static/css/main.7d047aa1.chunk.css"
+  },
+  {
+    "revision": "16503d581b394a50cc6d",
+    "url": "/static/js/2.6e35d388.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6e35d388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cea66aaa8cba6e88cea",
+    "url": "/static/js/main.668e0baf.chunk.js"
+  },
+  {
+    "revision": "7e6c1524872abf197a39",
+    "url": "/static/js/runtime-main.08521ef5.js"
+  }
+]);

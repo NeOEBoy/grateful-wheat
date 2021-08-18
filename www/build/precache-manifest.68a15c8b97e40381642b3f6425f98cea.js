e4b6b0c8850cda743da3fc7476db08751e1a1680
wangronghui <wangronghui2012@hotmail.com>
2021-08-18 19:41:45 +0800

    u

---
+++ www/build/precache-manifest.68a15c8b97e40381642b3f6425f98cea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3f9f4ee58687e330578451652b988d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08cf00b4ecd2f7e41361",
+    "url": "/static/css/2.8b2d98d4.chunk.css"
+  },
+  {
+    "revision": "3209e2dc2b433c76bb35",
+    "url": "/static/css/main.04cbb0e8.chunk.css"
+  },
+  {
+    "revision": "08cf00b4ecd2f7e41361",
+    "url": "/static/js/2.aae56d20.chunk.js"
+  },
+  {
+    "revision": "542c91f071fe828f7759f188e3ae7231",
+    "url": "/static/js/2.aae56d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3209e2dc2b433c76bb35",
+    "url": "/static/js/main.00dbd198.chunk.js"
+  },
+  {
+    "revision": "7e6c1524872abf197a39",
+    "url": "/static/js/runtime-main.08521ef5.js"
+  }
+]);

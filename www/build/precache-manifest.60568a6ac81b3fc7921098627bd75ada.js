f0a5e1ad7c36a3ce4d1d0aeedc0d95a3c4d3aa1b
wangronghui <wangronghui2012@hotmail.com>
2023-06-07 22:44:58 +0800

    update

---
+++ www/build/precache-manifest.60568a6ac81b3fc7921098627bd75ada.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca516fd3b55bf373cf4aed9d6e0632c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab02b6a8996719ad8b8",
+    "url": "/static/css/2.45609364.chunk.css"
+  },
+  {
+    "revision": "b8927fc080911af0e6a0",
+    "url": "/static/css/main.8325baae.chunk.css"
+  },
+  {
+    "revision": "8ab02b6a8996719ad8b8",
+    "url": "/static/js/2.cf211bbe.chunk.js"
+  },
+  {
+    "revision": "f3d2f49d1d7a920169ded93afdb566c6",
+    "url": "/static/js/2.cf211bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8927fc080911af0e6a0",
+    "url": "/static/js/main.933cd703.chunk.js"
+  },
+  {
+    "revision": "7e6c1524872abf197a39",
+    "url": "/static/js/runtime-main.08521ef5.js"
+  },
+  {
+    "revision": "0258c2a51cedb82313c45d696ea37d31",
+    "url": "/static/media/di.0258c2a5.wav"
+  },
+  {
+    "revision": "224b18a1145752f64a5f782b1cb7aadc",
+    "url": "/static/media/logo.224b18a1.png"
+  },
+  {
+    "revision": "09b8794b275b7f8f4a5a2153647b819b",
+    "url": "/static/media/shoppingCart.09b8794b.png"
+  },
+  {
+    "revision": "a0f6db3acdb8c6fac8555ed4f29cd2e1",
+    "url": "/static/media/教育局总店2号微信.a0f6db3a.jpg"
+  }
+]);
